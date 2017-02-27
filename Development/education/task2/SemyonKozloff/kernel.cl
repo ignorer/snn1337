1c1f5cf7243a88d29f3e8df23e4d4e2c7780648c
Semyon <semkozloff@gmail.com>
2017-02-27 16:25:49 +0300

    Replace spaces with tabs.

--- Development/education/task2/SemyonKozloff/kernel.cl
+++ Development/education/task2/SemyonKozloff/kernel.cl
@@ -3,9 +3,9 @@ __kernel void multiplier(__global int* a, __global int* b, __global int* c, __co
 	int dim = n[0];
 	int gid = get_global_id(0);
 	for (int k = 0; k < dim; ++k) {
-        c[gid * dim + k] = 0;
-        for (int i = 0; i < dim; ++i) {
-            c[gid * dim + k] += a[gid * dim + i] * b[i * dim + k];
-        }
-    }
+		c[gid * dim + k] = 0;
+		for (int i = 0; i < dim; ++i) {
+			c[gid * dim + k] += a[gid * dim + i] * b[i * dim + k];
+		}
+	}
 }
--- Development/education/task2/SemyonKozloff/main.cpp
+++ Development/education/task2/SemyonKozloff/main.cpp
@@ -14,32 +14,32 @@ const std::size_t N = 1000;
 const std::string fileName = "kernel.cl";
 
 void fillMatrix(cl_int* matrix) {
-    std::random_device randomDevice;
+	std::random_device randomDevice;
 	std::default_random_engine engine(randomDevice());
 	std::uniform_int_distribution<cl_int> uniformDistribution(1, 100);
 
-    for (std::size_t i = 0; i < N; ++i) {
-        for (std::size_t j = 0; j < N; ++j) {
-            matrix[i * N + j] = uniformDistribution(engine) % 10;
-        }
-    }
+	for (std::size_t i = 0; i < N; ++i) {
+		for (std::size_t j = 0; j < N; ++j) {
+			matrix[i * N + j] = uniformDistribution(engine) % 10;
+		}
+	}
 }
 
 void fillMatrix(cl_int* matrix, cl_int value) {
-    for (std::size_t i = 0; i < N; ++i) {
-        for (std::size_t j = 0; j < N; ++j) {
-            matrix[i * N + j] = value;
-        }
-    }
+	for (std::size_t i = 0; i < N; ++i) {
+		for (std::size_t j = 0; j < N; ++j) {
+			matrix[i * N + j] = value;
+		}
+	}
 }
 
 void printMatrix(cl_int* matrix) {
-    for (std::size_t i = 0; i < N; ++i) {
-        for (std::size_t j = 0; j < N; ++j) {
-            std::cout << matrix[i * N + j] << ' ';
-        }
-        std::cout << std::endl;
-    }
+	for (std::size_t i = 0; i < N; ++i) {
+		for (std::size_t j = 0; j < N; ++j) {
+			std::cout << matrix[i * N + j] << ' ';
+		}
+		std::cout << std::endl;
+	}
 }
 
 int main() {
@@ -60,24 +60,24 @@ int main() {
 	cl::STRING_CLASS buildInfoLog;
 
 	try {
-        std::vector<cl::Platform> availablePlatforms;
-        cl::Platform::get(&availablePlatforms);
-        if (availablePlatforms.size() == 0) {
-            throw cl::Error(1, "No available platforms.");
-        }
+		std::vector<cl::Platform> availablePlatforms;
+		cl::Platform::get(&availablePlatforms);
+		if (availablePlatforms.size() == 0) {
+			throw cl::Error(1, "No available platforms.");
+		}
 
-        cl_context_properties properties[] =
-                {CL_CONTEXT_PLATFORM, (cl_context_properties)(availablePlatforms[0])(), 0};
-        cl::Context context(CL_DEVICE_TYPE_GPU, properties);
-        std::vector<cl::Device> devices = context.getInfo<CL_CONTEXT_DEVICES>();
+		cl_context_properties properties[] =
+				{CL_CONTEXT_PLATFORM, (cl_context_properties)(availablePlatforms[0])(), 0};
+		cl::Context context(CL_DEVICE_TYPE_GPU, properties);
+		std::vector<cl::Device> devices = context.getInfo<CL_CONTEXT_DEVICES>();
 
-        cl::CommandQueue queue(context, devices[0]);
+		cl::CommandQueue queue(context, devices[0]);
 
-        std::ifstream file(fileName);
-        std::string fileContent(std::istreambuf_iterator<char>(file), {});
+		std::ifstream file(fileName);
+		std::string fileContent(std::istreambuf_iterator<char>(file), {});
 
-        cl::Program::Sources kernelSources{std::make_pair(fileContent.c_str(), fileContent.length())};
-        cl::Program program(context, kernelSources);
+		cl::Program::Sources kernelSources{std::make_pair(fileContent.c_str(), fileContent.length())};
+		cl::Program program(context, kernelSources);
 
 		try {
 			program.build(devices);
@@ -87,27 +87,27 @@ int main() {
 			throw cl::Error(error.err(), buildInfoLog.c_str());
 		}
 
-        cl::Kernel kernel(program, "multiplier");
+		cl::Kernel kernel(program, "multiplier");
 
-        std::size_t matrixMemorySize = N * N * sizeof(cl_int);
+		std::size_t matrixMemorySize = N * N * sizeof(cl_int);
 
-        cl::Buffer aBuffer(context, CL_MEM_READ_ONLY, matrixMemorySize);
-        cl::Buffer bBuffer(context, CL_MEM_READ_ONLY, matrixMemorySize);
-        cl::Buffer cBuffer(context, CL_MEM_READ_WRITE, matrixMemorySize);
-        cl::Buffer nBuffer(context, CL_MEM_READ_ONLY, sizeof(std::size_t));
-        queue.enqueueWriteBuffer(aBuffer, CL_TRUE, 0, matrixMemorySize, a.get());
-        queue.enqueueWriteBuffer(bBuffer, CL_TRUE, 0, matrixMemorySize, b.get());
-        queue.enqueueWriteBuffer(cBuffer, CL_TRUE, 0, matrixMemorySize, c.get());
-        queue.enqueueWriteBuffer(nBuffer, CL_TRUE, 0, sizeof(std::size_t), &N);
-        kernel.setArg(0, aBuffer);
-        kernel.setArg(1, bBuffer);
-        kernel.setArg(2, cBuffer);
-        kernel.setArg(3, nBuffer);
+		cl::Buffer aBuffer(context, CL_MEM_READ_ONLY, matrixMemorySize);
+		cl::Buffer bBuffer(context, CL_MEM_READ_ONLY, matrixMemorySize);
+		cl::Buffer cBuffer(context, CL_MEM_READ_WRITE, matrixMemorySize);
+		cl::Buffer nBuffer(context, CL_MEM_READ_ONLY, sizeof(std::size_t));
+		queue.enqueueWriteBuffer(aBuffer, CL_TRUE, 0, matrixMemorySize, a.get());
+		queue.enqueueWriteBuffer(bBuffer, CL_TRUE, 0, matrixMemorySize, b.get());
+		queue.enqueueWriteBuffer(cBuffer, CL_TRUE, 0, matrixMemorySize, c.get());
+		queue.enqueueWriteBuffer(nBuffer, CL_TRUE, 0, sizeof(std::size_t), &N);
+		kernel.setArg(0, aBuffer);
+		kernel.setArg(1, bBuffer);
+		kernel.setArg(2, cBuffer);
+		kernel.setArg(3, nBuffer);
 
-        cl::Event event;
+		cl::Event event;
 
 		auto start = std::chrono::steady_clock::now();
-        queue.enqueueNDRangeKernel(kernel, cl::NullRange, cl::NDRange(N), cl::NullRange, nullptr, &event);
+		queue.enqueueNDRangeKernel(kernel, cl::NullRange, cl::NDRange(N), cl::NullRange, nullptr, &event);
 		event.wait();
 		auto end = std::chrono::steady_clock::now();
 
@@ -116,14 +116,14 @@ int main() {
 		std::cout << elapsedMilliseconds << std::endl;
 
 		queue.enqueueReadBuffer(cBuffer, CL_TRUE, 0, matrixMemorySize, c.get());
-    }
-    catch (cl::Error& error) {
-        std::cerr << "ERROR: " << error.err() << std::endl;
-	    std::cerr << error.what() << std::endl;
-        return EXIT_FAILURE;
-    }
+	}
+	catch (cl::Error& error) {
+		std::cerr << "ERROR: " << error.err() << std::endl;
+		std::cerr << error.what() << std::endl;
+		return EXIT_FAILURE;
+	}
 
 	//::printMatrix(c.get());
 
-    return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
