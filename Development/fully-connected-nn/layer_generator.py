11e8a6e0204b5d25d30461386bc814215b866a89
Semyon <semkozloff@gmail.com>
2017-04-02 20:57:44 +0300

    Fix weights parameters passing in network_generator and layer_generator.

--- Development/fully-connected-nn/layer_generator.py
+++ Development/fully-connected-nn/layer_generator.py
@@ -2,46 +2,47 @@
 
 
 # num_outputs is actually number of neurons in the layer
-def generate_layer_code(bus_width, num_inputs, num_outputs, weight_vector):
-
+def generate_layer_code(bus_width, num_inputs, num_outputs):
     # module declaration
-    code = f'module layer{num_inputs}in{num_outputs}out(clk, rst, '
+    source = f'module layer{num_inputs}in{num_outputs}out(clk, rst, '
     for i in range(num_inputs):
-        code += f'in{i}, '
+        source += f'in{i}, '
     for i in range(num_outputs - 1):
-        code += f'out{i}, '
-    code += f'out{num_outputs - 1});\n\n'
-    'in{i}'
-    'out{i}'
+        source += f'out{i}, '
+    source += f'out{num_outputs - 1});\n\n'
+
     # parameters
-    for i in range(len(weight_vector)):
-        code += f'parameter W{i};\n'
-    code += '\n'
+    for i in range(num_inputs):
+        for j in range(num_outputs):
+            source += f'parameter W{i}TO{j};\n'
+    source += '\n'
 
     # ports definition
-    code += 'input wire clk;\ninput wire rst;\n\n'
+    source += 'input wire clk;\ninput wire rst;\n\n'
 
     for i in range(num_inputs):
-        code += f'input [{bus_width - 1}:0] in{i};\n'
-    code += '\n'
+        source += f'input [{bus_width - 1}:0] in{i};\n'
+    source += '\n'
 
     for i in range(num_outputs):
-        code += f'output [{bus_width - 1}:0] out{i};\n'
-    code += '\n'
+        source += f'output [{bus_width - 1}:0] out{i};\n'
+    source += '\n'
 
     # neurons
     for i in range(num_outputs):
-        code += f'neuron{num_inputs}in #('
+        source += f'neuron{num_inputs}in #('
         for j in range(num_inputs - 1):
-            code += f'.W{j}(W{j}), '
-        code += f'.W{num_inputs - 1}(W{num_inputs - 1})) '
-        code += f'neuron{i}(.clk(clk), .rst(rst), '
+            source += f'.W{j}(W{j}TO{i}), '
+        source += f'.W{num_inputs - 1}(W{num_inputs - 1}TO{i})) '
+        source += f'neuron{i}(.clk(clk), .rst(rst), '
         for j in range(num_inputs):
-            code += f'.in{j}(in{j}), '
-        code += f'.out(out{i}));\n'
-    code += '\n'
+            source += f'.in{j}(in{j}), '
+        source += f'.out(out{i}));\n'
+    source += '\n'
 
-    code += 'endmodule\n'
-    return code
+    source += 'endmodule\n'
+    return source
 
-print(generate_layer_code(bus_width=8, num_inputs=2, num_outputs=2, weight_vector=[2, 2]))
+
+print(generate_layer_code(bus_width=8, num_inputs=2, num_outputs=2))
+#print(generate_layer_code(bus_width=8, weight_matrix=np.matrix([[2, 2, 3], [3, 2, 2]])))
--- Development/fully-connected-nn/neuron_generator.py
+++ Development/fully-connected-nn/neuron_generator.py
@@ -1,23 +1,36 @@
 #!/usr/bin/python
 
 
-def neuron_generator(num_inputs):
-    input_wires = ['in{}'.format(i) for i in range(num_inputs)]
-
+def neuron_generator(bus_width, num_inputs):
     # module declaration
-    code = f'module neuron{num_inputs}in(clk, rst, '
+    source = f'module neuron{num_inputs}in(clk, rst, '
     for i in range(num_inputs):
-        code += f'in{i}, '
-    code += 'out);\n\n'
+        source += f'in{i}, '
+    source += 'out);\n\n'
 
     # parameters
     for i in range(num_inputs):
-        code += f'parameter W{i};\n'
-    code += '\n'
+        source += f'parameter W{i};\n'
+    source += '\n'
 
-    code += 'endmodule\n'
+    # ports definition
+    source += 'input wire clk;\ninput wire rst;\n\n'
 
-    return code
+    for i in range(num_inputs):
+        source += f'input [{bus_width - 1}:0] in{i};\n'
+    source += '\n'
+
+    source += f'output [{bus_width - 1}:0] out;\n\n'
+
+    # neuron logic
+    source += f'assign out = '
+    for i in range(num_inputs - 1):
+        source += f'in{i} * W{i} + '
+    source += f'in{num_inputs - 1} * W{num_inputs - 1};\n\n'
+
+    source += 'endmodule\n'
+
+    return source
 
 
-print(neuron_generator(num_inputs=2))
+print(neuron_generator(bus_width=8, num_inputs=4))
