2991ab4b2e06109ac2ca7a97748e8e12e4fe7625
Semyon <semkozloff@gmail.com>
2017-04-03 15:55:35 +0300

    Remove redundant files. Fin generate_neuron_module().

--- Development/fully-connected-nn/fcnn_generator.py
+++ Development/fully-connected-nn/fcnn_generator.py
@@ -119,7 +119,7 @@ class FCNNGenerator:
         source += 'endmodule\n'
         return source
 
-    def generate_neuron_module(self, num_inputs):
+    def generate_neuron_module(self, num_inputs, extended_width=32):
         # module declaration
         source = f'module neuron{num_inputs}in(clk, rst, '
         for i in range(num_inputs):
@@ -141,19 +141,20 @@ class FCNNGenerator:
         source += f'output reg [{self.bus_width - 1}:0] out;\n\n'
 
         # neuron logic
-        extended_width = 32
         source += f'reg signed [{extended_width - 1}:0] x;\n'
         source += f'reg [{extended_width - 1}:0] abs_x;\n'
+        source += f'reg [{extended_width - 1}:0] y;\n'
         source += 'always @* begin\n'
         source += '    x = '
         for i in range(num_inputs - 1):
             source += f'in{i} * W{i} + '
         source += f'in{num_inputs - 1} * W{num_inputs - 1};\n'
         source += '    abs_x = x < 0 ? -x : x;\n'
-        source += '    if (abs_x >= 5000) out = 1000;\n'
-        source += '    else if (abs_x >= 2375 && abs_x < 5000) out = 31 * abs_x + 843;\n'
-        source += '    else if (abs_x >= 1000 && abs_x < 2375) out = 125 * abs_x + 625;\n'
-        source += '    else if (abs_x >= 0 && abs_x < 1000) out = 250 * abs_x + 500;\n'
+        source += '    if (abs_x >= 5000) y = 1000;\n'
+        source += '    else if (abs_x >= 2375 && abs_x < 5000) y = 31 * abs_x + 843;\n'
+        source += '    else if (abs_x >= 1000 && abs_x < 2375) y = 125 * abs_x + 625;\n'
+        source += '    else if (abs_x >= 0 && abs_x < 1000) y = 250 * abs_x + 500;\n'
+        source += '    out = y / 1000;\n'
         source += 'end\n\n'
 
         source += 'endmodule\n'
