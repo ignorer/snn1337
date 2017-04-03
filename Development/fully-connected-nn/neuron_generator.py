7f8b06db42de3fb8603158af80cf48ada0d2fb9c
Semyon <semkozloff@gmail.com>
2017-04-03 12:44:46 +0300

    Add approximator of activation function.

--- Development/fully-connected-nn/neuron_generator.py
+++ Development/fully-connected-nn/neuron_generator.py
@@ -21,17 +21,27 @@ def neuron_generator(bus_width, num_inputs):
         source += f'input [{bus_width - 1}:0] in{i};\n'
     source += '\n'
 
-    source += f'output [{bus_width - 1}:0] out;\n\n'
+    source += f'output reg [{bus_width - 1}:0] out;\n\n'
 
     # neuron logic
-    source += 'assign out = '
+    extended_width = 32
+    source += f'reg signed [{extended_width - 1}:0] x;\n'
+    source += f'reg [{extended_width - 1}:0] abs_x;\n'
+    source += 'always @* begin\n'
+    source += '    x = '
     for i in range(num_inputs - 1):
         source += f'in{i} * W{i} + '
-    source += f'in{num_inputs - 1} * W{num_inputs - 1};\n\n'
+    source += f'in{num_inputs - 1} * W{num_inputs - 1};\n'
+    source += '    abs_x = x < 0 ? -x : x;\n'
+    source += '    if (abs_x >= 5000) out = 1000;\n'
+    source += '    else if (abs_x >= 2375 && abs_x < 5000) out = 31 * abs_x + 843;\n'
+    source += '    else if (abs_x >= 1000 && abs_x < 2375) out = 125 * abs_x + 625;\n'
+    source += '    else if (abs_x >= 0 && abs_x < 1000) out = 250 * abs_x + 500;\n'
+    source += 'end\n\n'
 
     source += 'endmodule\n'
 
     return source
 
 
-print(neuron_generator(bus_width=8, num_inputs=4))
+print(neuron_generator(bus_width=8, num_inputs=16))
